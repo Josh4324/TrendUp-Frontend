0d10b2d93956baf1a34d459ec4b8c9fc8164ef88
Joshua Adesanya <adesanyajoshua@ymail.com>
2021-07-27 16:50:47 +0100

    new changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const http = "http://localhost:8080";
-//export const http = "http://staging.trendupp.com";
+//export const http = "http://localhost:8080";
+export const http = "http://staging.trendupp.com";
 
-export const front = "http://localhost:3001";
-//export const front = "http://staging.trendupp.com";
+//export const front = "http://localhost:3001";
+export const front = "http://staging.trendupp.com";
